5a3c3f5047b686666bc42679c8ea6869d4ca61e8
jaco9419 <jaco.9419@gmail.com>
2020-10-28 01:20:37 -0300

    Rearranged order of files

--- javascript/main.js
+++ javascript/main.js
@@ -24,19 +24,19 @@ const headerInner = `
 const navInner = `
         <ul>
             <li>
-                <a href="${home ? '' : 'index.html'}#header" class="hover" >Home</a>
+                <a href="${home ? '' : '../index.html'}#header" class="hover" >Home</a>
             </li>
             <li>
-                <a href="${home ? '' : 'index.html'}#about" class="hover" >Sobre Tu Vivero</a>
+                <a href="${home ? '' : '../index.html'}#about" class="hover" >Sobre Tu Vivero</a>
             </li>
             <li>
-                <a href="productos.html" class="hover" >Productos</a>
+                <a href="${home ? '/html/' : ''}productos.html" class="hover" >Productos</a>
             </li>
             <li>
-                <a href="contacto.html" class="hover" >Contacto</a>
+                <a href="${home ? '/html/' : ''}contacto.html" class="hover" >Contacto</a>
             </li>
             <li>
-                <a href="sucursales.html" class="hover" >Sucursales</a>
+                <a href="${home ? '/html/' : ''}sucursales.html" class="hover" >Sucursales</a>
             </li>
         </ul>
 `;
